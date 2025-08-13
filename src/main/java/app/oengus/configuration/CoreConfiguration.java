47b89fa7c0cee4947b5ce20aee2ba7893190341e
Gyoo <gaetan.young@gmail.com>
2025-08-13 10:05:52 +0200

    Cors shenanigans

--- src/main/java/app/oengus/configuration/CoreConfiguration.java
+++ src/main/java/app/oengus/configuration/CoreConfiguration.java
@@ -24,7 +24,7 @@ public class CoreConfiguration implements WebMvcConfigurer {
             .exposedHeaders("Location", "Origin")
             // FUCK https://stackoverflow.com/questions/48712638/cors-for-spring-boot-not-in-response-header
             .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
-            .allowedOrigins("*")
+            .allowedOrigins("http://localhost:4200")
             .allowCredentials(false)
             .allowedHeaders("*");
     }
